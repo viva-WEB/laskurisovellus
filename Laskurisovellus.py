80054f023a828ab45488558e41e2f68d711bd0b0
virvaldev <valtteri.virtanen.dev@gmail.com>
2023-12-18 10:25:14 +0200

    Alkuperäinen commit

---
+++ Laskurisovellus.py
@@ -0,0 +1,37 @@
+# Ikälaskuri
+# 18.12.2023 Valtteri Virtanen, GitHub: https://github.com/viva-WEB/ikalaskuri
+
+# Globaalit muuttujat
+risuaita = "#" * 25
+rivinvaihto = "\n" * 2
+
+# Aliohjelma tervehdyksen tulostamiseen
+def tulosta_tervehdys():
+    print(risuaita, "Tervetuloa ikälaskuriin!", risuaita, sep="\n", end=rivinvaihto)
+    print("Ohjelma laskee ikäsi annetulla tarkkuudella syntymäaikasi perusteella.", end=rivinvaihto)
+        
+    print("Ohjelman on kirjoitettu Python-perusteisiin ja koodin rakenteisiin keskittyvänä harjoitustyönä joulukuussa 2023",
+        "Ohjelma on avoimen lähdekoodin ohjelma. Levitys ja muokkaus on vapaasti sallittua.", sep="\n", end=rivinvaihto)
+    print(risuaita, "Valtteri Virtanen (2023)",
+        "https://github.com/viva-WEB/", risuaita, sep="\n", end=rivinvaihto) 
+    print(risuaita)
+
+    return None
+
+# Aliohjelma päävalikolle
+def valikko():
+
+    return None
+
+
+# Pääohjelma
+def main():
+    # Tervehdys aliohjelma
+    tulosta_tervehdys()
+    valikko()
+
+    print("Kiitos ohjelman käytöstä.")
+    return None
+
+
+main()
