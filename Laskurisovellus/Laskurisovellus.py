bb123aec0326f5908f578e31b7c98ce1c140a634
virvaldev <valtteri.virtanen.dev@gmail.com>
2023-12-18 16:20:01 +0200

    IK-5-Luo-valikkorakenne Paikanvaihto

---
+++ Laskurisovellus/Laskurisovellus.py
@@ -0,0 +1,64 @@
+# Ikälaskuri
+# 18.12.2023 Valtteri Virtanen, GitHub: https://github.com/viva-WEB/ikalaskuri
+
+# Globaalit muuttujat
+risuaita = "#" * 26
+rivinvaihto = "\n" * 2
+
+# Aliohjelma tervehdyksen tulostamiseen
+def tulosta_tervehdys():
+    print(risuaita, "Tervetuloa Laskurisovellukseen!", risuaita, sep="\n", end=rivinvaihto)
+    print("Sovellus sisältää joukon eri laskureita ja muuntimia.", end=rivinvaihto)
+        
+    print("Ohjelman on kirjoitettu Python-perusteisiin ja koodin rakenteisiin keskittyvänä harjoitustyönä joulukuussa 2023",
+        "Ohjelma on avoimen lähdekoodin ohjelma. Levitys ja muokkaus on vapaasti sallittua.", sep="\n", end=rivinvaihto)
+    print("Valtteri Virtanen (2023)",
+        "https://github.com/viva-WEB/", risuaita, sep="\n", end=rivinvaihto) 
+    
+    syote = input("Paina Enter jatkaaksesi...")
+    print(risuaita, end=rivinvaihto)
+    return None
+
+# Aliohjelma päävalikolle
+def valikko():
+    while True:
+        print(risuaita, "\t" + "PÄÄVALIKKO", risuaita, sep="\n", end=rivinvaihto)
+
+        print("Valitse toiminto valitsemalla numero valikosta.", end=rivinvaihto)
+        print("1. Painoindeksilaskuri")
+        print("2. Ikälaskuri")
+        print("3. Kuinka monta päivää jouluun?")
+        print("4. Kuinka monta päivää synttäreihin?")
+        print("5. Yksikkömuunnin", end=rivinvaihto)
+     
+        print("0. Lopeta ohjelma", end=rivinvaihto)
+        
+        syote = input("Valitse toiminto: ")
+        valinta = int(syote)
+
+        if valinta == 1:
+            painoindeksilaskuri()
+        else:
+            break
+        
+            
+        break
+
+
+    return None
+
+
+# Pääohjelma
+def main():
+    # Tervehdys aliohjelma
+    tulosta_tervehdys()
+    valikko()
+
+    print("Kiitos ohjelman käytöstä.")
+    return None
+
+
+main()
+
+
+
