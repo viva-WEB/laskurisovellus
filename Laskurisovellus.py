f3ed3609c649134d12c46af1d97cb217e07e8df8
virvaldev <valtteri.virtanen.dev@gmail.com>
2023-12-18 16:12:28 +0200

    IK-5 Valikkorakenteen luuranko.

--- Laskurisovellus.py
+++ Laskurisovellus.py
@@ -2,24 +2,39 @@
 # 18.12.2023 Valtteri Virtanen, GitHub: https://github.com/viva-WEB/ikalaskuri
 
 # Globaalit muuttujat
-risuaita = "#" * 25
+risuaita = "#" * 26
 rivinvaihto = "\n" * 2
 
 # Aliohjelma tervehdyksen tulostamiseen
 def tulosta_tervehdys():
-    print(risuaita, "Tervetuloa ikälaskuriin!", risuaita, sep="\n", end=rivinvaihto)
-    print("Ohjelma laskee ikäsi annetulla tarkkuudella syntymäaikasi perusteella.", end=rivinvaihto)
+    print(risuaita, "Tervetuloa Laskurisovellukseen!", risuaita, sep="\n", end=rivinvaihto)
+    print("Sovellus sisältää joukon eri laskureita ja muuntimia.", end=rivinvaihto)
         
     print("Ohjelman on kirjoitettu Python-perusteisiin ja koodin rakenteisiin keskittyvänä harjoitustyönä joulukuussa 2023",
         "Ohjelma on avoimen lähdekoodin ohjelma. Levitys ja muokkaus on vapaasti sallittua.", sep="\n", end=rivinvaihto)
-    print(risuaita, "Valtteri Virtanen (2023)",
+    print("Valtteri Virtanen (2023)",
         "https://github.com/viva-WEB/", risuaita, sep="\n", end=rivinvaihto) 
-    print(risuaita)
-
+    
+    syote = input("Paina Enter jatkaaksesi...")
+    print(risuaita, end=rivinvaihto)
     return None
 
 # Aliohjelma päävalikolle
 def valikko():
+    while True:
+        print(risuaita, "\t" + "PÄÄVALIKKO", risuaita, sep="\n", end=rivinvaihto)
+
+        print("Valitse toiminto valitsemalla numero valikosta.", end=rivinvaihto)
+        print("1. Painoindeksilaskuri")
+        print("2. Ikälaskuri")
+        print("3. Kuinka monta päivää jouluun?")
+        print("4. Kuinka monta päivää synttäreihin?")
+        print("5. Yksikkömuunnin")
+        print()
+        print("0. Lopeta ohjelma")
+
+        break
+
 
     return None
 
@@ -35,3 +50,5 @@ def main():
 
 
 main()
+
+
